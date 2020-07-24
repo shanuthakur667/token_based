e1188c013228885e657e6e910906bbed223ef37d
yugansh-thakur <yugansh.kumar@reddoorz.com>
2020-07-24 12:25:21 +0530

    validation changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   has_many :cart_products
   has_many :carts, through: :cart_products
 
-  validates :name, :client_id, :registration_date, :country_id, :territory_id, :area, :client_type, presence: true
+  validates :name, :description, :make, presence: true
 end
